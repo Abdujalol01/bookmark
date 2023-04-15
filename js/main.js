4910b9766a4f7855289913baa047d9e8ee29ae77
Abdujalol01 <jalolhalimov203@gmail.com>
2023-04-16 02:37:23 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -1,43 +1,33 @@
-const tabsPanel = document.querySelectorAll(".tabs__panel") ,
-tabsItem = document.querySelector(".tabs__items"),
-tabLinks = document.querySelectorAll(".tabs__link"),
-tabsList = document.querySelectorAll(".tabs__item"),
-elToggler = document.querySelectorAll(".accordion__item--toggler"),
-accordionContent = document.querySelectorAll(".accordion__item--content");
-
-
-
-function close(){
-  tabsPanel.forEach(item =>{
-    item.classList.add("hide");
-    item.classList.remove("show");
-  });
-  tabsList.forEach(item=>{
-    item.classList.remove("tabs__link--active");
-  });
-}
-
-function open( i=0){
-  tabsPanel[i].classList.add("show");
-  tabsPanel[i].classList.remove("hide");
-  tabsList[i].classList.add("tabs__link--active");
-}
-tabsItem.addEventListener("click", (event)=>{
-  const target= event.target;
-  if(target &&target.classList.contains("tabs__link")){
-    tabLinks.forEach((item,idx)=>{
-      if(target == item){
-        close();
-        open(idx);
-      }
+  "use strict";
+  const tabsParent = document.querySelector(".tabs__items"),
+  tabs = document.querySelectorAll(".tabs__link"),
+  panels = document.querySelectorAll(".tabs__panel"),
+  elItems = document.querySelectorAll(".tabs__item");
+  
+  function hideContent (){
+    panels.forEach(item =>{
+      item.classList.add("hide");
+      item.classList.remove("show");
+    });
+    elItems.forEach(item=>{
+      item.classList.remove("tabs__link--active");
     });
   }
-});
-
-elToggler.forEach(item=>{
-  item.addEventListener("click" , ()=>{
-    accordionContent.forEach(item=>{
-      item.classList.toggle("show");
-    });
+  
+  function showContent(i=0){
+    panels[i].classList.add("show");
+    panels[i].classList.remove("hide");
+    elItems[i].classList.add("tabs__link--active");
+  }
+  
+  tabsParent.addEventListener("click" , (event)=>{
+    const target = event.target;
+    if(target && target.classList.contains("tabs__link")){
+      tabs.forEach((item,idx)=>{
+        if(target==item){
+          hideContent();
+          showContent(idx);
+        }
+      });
+    }
   });
-});
